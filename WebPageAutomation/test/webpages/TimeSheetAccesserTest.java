98d43fd0fe986405588ae62dc9987c81309eda42
fpenovi <fpenovi@certant.com>
2017-01-18 14:29:19 -0300

    Clase UserMock genera un usuario valido a partir de un archivo .properties

--- WebPageAutomation/test/webpages/TimeSheetAccesserTest.java
+++ WebPageAutomation/test/webpages/TimeSheetAccesserTest.java
@@ -1,14 +1,22 @@
 package webpages;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import model.Configuration;
 import model.FileManager;
 import model.User;
 import model.exceptions.IncorrectUsernameOrPasswordException;
 import model.webpages.timesheet.TimeSheetAccesser;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.Assert;
 
 
 public class TimeSheetAccesserTest {
@@ -16,6 +24,7 @@ public class TimeSheetAccesserTest {
 	private TimeSheetAccesser webPageAccesser;
 	private Configuration config;
 	private User user;
+	private String resourceFolder = File.separator + "resources";
 	
 	@Before
 	public void createWebPageAcceser() {
@@ -45,7 +54,7 @@ public class TimeSheetAccesserTest {
 		
 		config = new Configuration();
 		user = new User(config);
-		MockUser validUser = new MockUser();
+		UserMock validUser = new UserMock();
 		user.setUserName(validUser.getUserName());
 		user.setPassword(validUser.getPassword());
 		webPageAccesser.setUser(user);
@@ -54,6 +63,44 @@ public class TimeSheetAccesserTest {
 		
 		String urlNextPage = webPageAccesser.getCurrentPage();		
 		Assert.assertEquals("https://webi.certant.com/timesheet/calendar.php", urlNextPage);
+	}
+	
+	
+	private class UserMock {
+		
+		String userName;
+		String password;
+		String fileName = "valid-user.properties";
+		final String seed = "mock-valid-user";
+		
+		UserMock() {
+			String filePath = new File("").getAbsolutePath() + resourceFolder + File.separator + fileName;
+			
+			Properties prop = new Properties();
+			InputStream input = null;
+			
+			try {
+				input = new FileInputStream(filePath);
+				prop.load(input);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+			encryptor.setPassword(seed);
+
+			this.userName = encryptor.decrypt(prop.getProperty("username"));
+			this.password = encryptor.decrypt(prop.getProperty("password"));
+		}
+		
+		
+		String getUserName() {
+			return this.userName;			
+		}
+		
+		String getPassword() {
+			return this.password;
+		}
 	}
 
 }
