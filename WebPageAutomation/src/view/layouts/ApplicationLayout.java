923fa5db2cde74e5dc90cf97db7f89646f04c7a9
fpenovi <fpenovi@certant.com>
2017-01-20 18:20:52 -0300

    No se ni que hice en este commit

--- WebPageAutomation/src/view/layouts/ApplicationLayout.java
+++ WebPageAutomation/src/view/layouts/ApplicationLayout.java
@@ -39,13 +39,14 @@ public abstract class ApplicationLayout extends BorderPane {
 		this.title = new Label();
 		this.description = new Text();
 		this.description.setWrappingWidth(300);
-		VBox vbTextContainer = new VBox(10);
+		VBox vbTextContainer = new VBox();
 		vbTextContainer.setPadding(new Insets(20, 0, 0, 20));
 		vbTextContainer.getChildren().addAll(this.title, this.description);
 		this.title.setId("header-title");
 		this.description.setId("header-description");
 		this.header.getChildren().add(vbTextContainer);
-		// this.getStylesheets().add(getClass().getResource("application-container.css").toExternalForm());
+		this.getStylesheets().add(getClass().getResource("/CSS/application-container.css").toExternalForm());
+		// this.header.setStyle("-fx-background-color: #7b9bce;");
 	}
 
 }
